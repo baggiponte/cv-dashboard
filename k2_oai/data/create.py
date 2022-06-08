82d9b08406ccd848177a39e2f0ed8687b0b8bc49
baggiponte <lucabaggi.uni@gmail.com>
2022-06-08 10:34:35 +0200

    added func to concat annotations checkpoints

--- k2_oai/data/create.py
+++ k2_oai/data/create.py
@@ -3,8 +3,10 @@ Functions to manipulate the raw data.
 """
 
 import geopandas
+import pandas as pd
 
-from k2_oai.data.load import dbx_load_metadata
+from k2_oai import dropbox as dbx
+from k2_oai.data.load import dbx_load_dataframe, dbx_load_metadata
 
 __all__ = [
     "dbx_create_geo_metadata",
@@ -22,4 +24,23 @@ def dbx_create_geo_metadata(dropbox_app):
         metadata,
         geometry=geopandas.points_from_xy(metadata.lon, metadata.lat),
         crs=4326,
+    )
+
+
+def dbx_concat_label_annotations(dropbox_app):
+    files = dbx.dropbox_listdir("/k2/metadata/label_annotations", dropbox_app).item_name
+    checkpoints = [
+        dbx_load_dataframe(file, "/k2/metadata/label_annotations", dropbox_app)
+        for file in files
+        if "-checkpoint-" in file
+    ]
+
+    return (
+        pd.concat(checkpoints)
+        .sort_values(["roof_id", "annotation_time"])
+        .drop_duplicates(subset="roof_id", keep="last")
+        .rename(columns={"annotation": "is_trainable"})
+        .assign(
+            photos_folder=lambda df: df.photos_folder.str.replace("-api_upload", "")
+        )
     )
